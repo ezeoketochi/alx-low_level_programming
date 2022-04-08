27aff6f47183b13e343ebc22d4c135a5836be1cf
ezeoketochi <tochioluebube@gmail.com>
2022-04-08 15:53:18 -0700

    trying again

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -6,15 +6,19 @@
  * * Get a ramdom number and print the number
  * * and if it positive, negative, or zero
  * * Return: 0
- * */
+ * /
+
 int main(void)
 {
 	int n;
 
-
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 		if (n > 0)
-			printf("%d is positive\n", n);				  else if (n < 0)							    printf("%d is negative\n", n);			      else
-		        printf("%d is zero\n", n);			     return (0);
+			printf("%d is positive\n", n);
+		else if(n < 0)
+			printf("%d is negative\n", n);
+		else
+			printf("%d is zero\n", n);
+	return (0);
 }
