2f3311182d737fd1b5d81672d7506b6c80f80b7b
Ezeoke Tochi <66806637+ezeoketochi@users.noreply.github.com>
2022-04-12 05:25:08 +0100

    Update 2-print_alphabet.c
    correction

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,23 +1,16 @@
-
-  
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <time.h>
 /**
- * main - Entry point
- *
- * Return: 0
- */
+*main - main block
+*Program prints alphabet in lowcase, followed by a new line
+*Return: 0
+*/
 int main(void)
 {
-	char a = 'a';
-
-	while (a <= 'z')
-	{
-		putchar(a);
-		a++;
-	}
-
-	putchar('\n');
+	char ch = 'a';
+		for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+		putchar('\n');
 	return (0);
 }
